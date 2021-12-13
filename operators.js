09401d4d0c796e6bfbf7f5f78cb0dadfca6090a5
Mounika <bojjamounika8@gmail.com>
2021-12-13 22:28:29 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,34 @@
+var x=20;
+var y=20;
+var add=x+y;
+console.log(add);
+
+var x=40;
+var y=10;
+var sub=x-y;
+console.log(sub);
+
+var x=50;
+var y=10;
+var mul=x*y;
+console.log(mul);
+
+var x=40;
+var y=10;
+var div=x/y;
+console.log(div);
+
+var x=5;
+var y=2;
+var modulus=x%y;
+console.log(modulus);
+
+var x=7; 
+x++;
+var z=x;
+console.log(z);
+
+var y=8;
+y--;
+var z=y;
+console.log(z);
